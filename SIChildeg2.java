60843ba647b15f9078c6f8aaeed1833edc112b4b
AryanSrivastava111 <122785189+AryanSrivastava111@users.noreply.github.com>
2023-02-16 15:27:32 +0530

    Add files via upload

---
+++ SIChildeg2.java
@@ -0,0 +1,15 @@
+class SIParenteg2{// parent class
+	 int salary = 30000; // parent class member
+ }
+public class SIChildeg2  extends SIParenteg2 {
+	int bonus=1500;
+	int annualsal= salary+bonus;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SIChildeg2 object = new SIChildeg2();
+		System.out.println("Salary is:"+object);
+		System.out.println("Annual Salary is:"+ object.annualsal);
+		
+	}
+
+}
